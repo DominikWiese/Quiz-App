c149744bc750bb18dda71d43ae17e93f16655349
DominikWiese <dominikwiese81@gmail.com>
2023-03-28 11:35:02 +0200

    js and json added

---
+++ script.js
@@ -0,0 +1,96 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Frage",
+        "answer_1": "Antwort",
+        "answer_2": "Antwort",
+        "answer_3": "Antwort",
+        "answer_4": "Antwort",
+        "right_answer": 3
+    },
+    {
+        "question": "Frage",
+        "answer_1": "Antwort",
+        "answer_2": "Antwort",
+        "answer_3": "Antwort",
+        "answer_4": "Antwort",
+        "right_answer": 3
+    },
+    {
+        "question": "Frage",
+        "answer_1": "Antwort",
+        "answer_2": "Antwort",
+        "answer_3": "Antwort",
+        "answer_4": "Antwort",
+        "right_answer": 3
+    },
+    {
+        "question": "Frage",
+        "answer_1": "Antwort",
+        "answer_2": "Antwort",
+        "answer_3": "Antwort",
+        "answer_4": "Antwort",
+        "right_answer": 3
+    },
+    {
+        "question": "Frage",
+        "answer_1": "Antwort",
+        "answer_2": "Antwort",
+        "answer_3": "Antwort",
+        "answer_4": "Antwort",
+        "right_answer": 3
+    },
+    {
+        "question": "Frage",
+        "answer_1": "Antwort",
+        "answer_2": "Antwort",
+        "answer_3": "Antwort",
+        "answer_4": "Antwort",
+        "right_answer": 3
+    },
+];
+
+
+let currentQuestion = 0;
+
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    if(selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        
+    }
+
+    document.getElementById('next-button').disabled = false;
+}
