9c4d2423eedebe94b8d87e34dfea6a6547c51b18
DominikWiese <dominikwiese81@gmail.com>
2023-03-30 23:47:02 +0200

    change sounds and added end sound, new images, and questions added

--- script.js
+++ script.js
@@ -1,66 +1,221 @@
 let questions = [
     {
-        "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
+        "question": "Sheldon nimmt das Sortieren seiner Frühstücksflocken sehr ernst. Wonach sortiert er die Cornflakes?",
+        "answer_1": "Nach Größe",
+        "answer_2": "Alphabetisch",
+        "answer_3": "Ballaststoffgehalt",
         "right_answer": 3
     },
     {
-        "question": "Frage",
-        "answer_1": "Antwort",
-        "answer_2": "Antwort",
-        "answer_3": "Antwort",
-        "answer_4": "Antwort",
+        "question": "Sheldon besitzt mehr als einen akademischen Abschluss. Wisst ihr wie viele er hat?",
+        "answer_1": "5",
+        "answer_2": "7",
+        "answer_3": "9",
+        "right_answer": 1
+    },
+    {
+        "question": "Leonard spielt ein klassisches Instroment. Wisst ihr noch welches?",
+        "answer_1": "Klarinette",
+        "answer_2": "Geige",
+        "answer_3": "Cello",
         "right_answer": 3
     },
     {
-        "question": "Frage",
-        "answer_1": "Antwort",
-        "answer_2": "Antwort",
-        "answer_3": "Antwort",
-        "answer_4": "Antwort",
+        "question": "Amy und Sheldon kreieren eine YouTube Serie zusammen. Wie heißt sie?",
+        "answer_1": "Flaggenspaß",
+        "answer_2": "Flaggen der Welt",
+        "answer_3": "Spaß mit Flaggen",
         "right_answer": 3
     },
     {
-        "question": "Frage",
-        "answer_1": "Antwort",
-        "answer_2": "Antwort",
-        "answer_3": "Antwort",
-        "answer_4": "Antwort",
+        "question": "Wo ist Penny aufgewachsen?",
+        "answer_1": "Pennsylvania",
+        "answer_2": "Nebraska",
+        "answer_3": "Minnesota",
+        "right_answer": 2
+    },
+    {
+        "question": "Beim inoffiziellen Roboter-Turnier muss sich M.O.N.T.E, der Roboter der Jungs, gegen den von Barry Kripke behaupten. Wisst ihr noch wie seiner heißt?",
+        "answer_1": "Kripke Krippler",
+        "answer_2": "Kripke Killer",
+        "answer_3": "Kripke Mangler",
+        "right_answer": 1
+    },
+    {
+        "question": "Eines der 'Spice Girls' mochte Penny mehr als die anderen, wen?",
+        "answer_1": "Sporty",
+        "answer_2": "Baby",
+        "answer_3": "Ginger",
+        "right_answer": 2
+    },
+    {
+        "question": "Eine Person, die bei TBBT mitspielt, hat einen echten Doktortitel in Neurobiologie. Wer?",
+        "answer_1": "Mayim Bialik (Amy)",
+        "answer_2": "Johnny Galecki (Leonard)",
+        "answer_3": "Jim Parsons (Sheldon)",
+        "right_answer": 1
+    },
+    {
+        "question": "Sheldon trägt oft T-Shirts von The Flash. In der TV-Serie 'The Flash' wird sich dafür 'bedankt'. Wie?",
+        "answer_1": "Iris zitiert aus TBBT",
+        "answer_2": "Cisco trägt TBBT Shirts",
+        "answer_3": "Barry sagt Bazinga",
+        "right_answer": 2
+    },
+    {
+        "question": "Sheldon und Amy werden für den Nobelpreis vorgeschlagen. Diesen teilen sie sich mit zwei weiteren Personen. Wie heißen die beiden?",
+        "answer_1": "Dr. Astin und Dr. Penn",
+        "answer_2": "Dr. Pimbertonn und Dr. Kamppell",
+        "answer_3": "Dr. Pemberton und Dr. Campbell",
         "right_answer": 3
     },
     {
-        "question": "Frage",
-        "answer_1": "Antwort",
-        "answer_2": "Antwort",
-        "answer_3": "Antwort",
-        "answer_4": "Antwort",
+        "question": "Einer der Stars von TBBT ist stolzer Preisträger einer Goldenen Himbeere. Wer?",
+        "answer_1": "Melissa Rauch",
+        "answer_2": "Kaley Cuoco",
+        "answer_3": "Johnny Galecki",
+        "right_answer": 2
+    },
+    {
+        "question": "Nachdem es bei Raj mit der Ehe nicht klappt, lässt er eine arrangieren. Wen soll er heiraten?",
+        "answer_1": "Anu",
+        "answer_2": "Ruchi",
+        "answer_3": "Priya",
+        "right_answer": 1
+    },
+    {
+        "question": "The Big Bang Theory endet 2019. Wie viele Staffeln hat die Comedy-Serie geschafft?",
+        "answer_1": "11",
+        "answer_2": "12",
+        "answer_3": "13",
+        "right_answer": 2
+    },
+    {
+        "question": "Von den vier Nerds hat lediglich einer keinen Doktor-Titel. Wer?",
+        "answer_1": "Howard",
+        "answer_2": "Leonard",
+        "answer_3": "Sheldon",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche der folgenden Damen war nie mit Raj zusammen?",
+        "answer_1": "Lucy",
+        "answer_2": "Amy",
+        "answer_3": "Emily",
+        "right_answer": 2
+    },
+    {
+        "question": "Immer wenn Sheldon einen Witz macht, sagt er ein bestimmtes Wort. Welches?",
+        "answer_1": "Booyah!",
+        "answer_2": "Bazinga!",
+        "answer_3": "Bingo!",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Musikinstroment spielt Amy Farrah Fowler?",
+        "answer_1": "Oboe",
+        "answer_2": "Klarinette",
+        "answer_3": "Harfe",
         "right_answer": 3
     },
     {
-        "question": "Frage",
-        "answer_1": "Antwort",
-        "answer_2": "Antwort",
-        "answer_3": "Antwort",
-        "answer_4": "Antwort",
+        "question": "In welcher Episode kam das erste gemeinsame Kind von Bernadette und Howard zur Welt?",
+        "answer_1": "Die Neuvermessung der Liebe",
+        "answer_2": "Kick it like Baby",
+        "answer_3": "Immer zum Geburtstag",
         "right_answer": 3
     },
     {
-        "question": "Frage",
-        "answer_1": "Antwort",
-        "answer_2": "Antwort",
-        "answer_3": "Antwort",
-        "answer_4": "Antwort",
+        "question": "Bei welchem seiner Projekte passierte Howard ein Fehler, mit dem er immer wieder von seinen Freunden aufgezogen wird?",
+        "answer_1": "Er baute eine Weltraumdusche",
+        "answer_2": "Er baute eine Weltraumtoilette",
+        "answer_3": "Er entwickelte ein Laserschwert",
+        "right_answer": 2
+    },
+    {
+        "question": "Auch Bernadette hat einen Doktortitel. was ist ihr Fachgebiet?",
+        "answer_1": "Physik",
+        "answer_2": "Neurobiologie",
+        "answer_3": "Mikrobiologie",
         "right_answer": 3
+    },
+    {
+        "question": "Mit was verdienen Leonard und Sheldon ihren Lebensunterhalt?",
+        "answer_1": "Physiker",
+        "answer_2": "Biologen",
+        "answer_3": "Chemiker",
+        "right_answer": 1
+    },
+    {
+        "question": "Der Fahrstuhl im Wohnhaus von Sheldon und Leonard war nicht immer kaputt. Wer hat ihn kaputt gemacht?",
+        "answer_1": "Howard",
+        "answer_2": "Leonard",
+        "answer_3": "Sheldon",
+        "right_answer": 3
+    },
+    {
+        "question": "In welchem Staat leben Howard, Penny und Co?",
+        "answer_1": "New York",
+        "answer_2": "Kalifornien",
+        "answer_3": "Nebraska",
+        "right_answer": 2
+    },
+    {
+        "question": "Penny war nicht immer für ein Pharmaunternehmen tätig. Wo war sie vorher?",
+        "answer_1": "Muffins Place",
+        "answer_2": "Hot Dog Heaven",
+        "answer_3": "Cheesecake Factory",
+        "right_answer": 3
+    },
+    {
+        "question": "In welchem Institut arbeiten die beiden Physiker?",
+        "answer_1": "Caltech",
+        "answer_2": "MIT",
+        "answer_3": "UCLA",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie heißt der Comicverkäufer, bei dem die Nerds oft zu Besuch sind?",
+        "answer_1": "Stuart",
+        "answer_2": "Will Weaton",
+        "answer_3": "Steven",
+        "right_answer": 1
+    },
+    {
+        "question": "Howard beherrscht eine bekannte Fantasysprache. Welche?",
+        "answer_1": "Elbisch",
+        "answer_2": "Klingonisch",
+        "answer_3": "Dothrakisch",
+        "right_answer": 2
+    },
+    {
+        "question": "Sheldon ist nicht einfach nur schlau. Er hat auch eine bestimmte Fähigkeit, die ihm dabei hilft.",
+        "answer_1": "Er hat ein fotografisches Gedächtnis",
+        "answer_2": "Er hat Tetrachromasie",
+        "answer_3": "Er kann in die Zukunft sehen",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie lautet Pennys Nachname? War sie vielleicht schon einmal verheiratet?",
+        "answer_1": "Wilson",
+        "answer_2": "Johnson",
+        "answer_3": "Das weiß niemand",
+        "right_answer": 3
+    },
+    {
+        "question": "Leonard ist ziemlich schlau. Wie hoch ist sein IQ?",
+        "answer_1": "173",
+        "answer_2": "203",
+        "answer_3": "154",
+        "right_answer": 1
     },
 ];
 
 let rightQuestions = 0;
 let currentQuestion = 0;
 let audio_success = new Audio('audio/success.mp3');
-let audio_fail = new Audio('audio/wrong.mp3');
+let audio_fail = new Audio('audio/wrong1.mp3');
+let audio_end_game = new Audio('audio/win.mp3');
 
 
 function init() {
@@ -84,11 +239,12 @@ function gameIsOver() {
 }
 
 function showEndScreen(){
+    audio_end_game.play();
     document.getElementById('endScreen').style = '';
     document.getElementById('questionBody').style = 'display: none';
     document.getElementById('endAllQuestion').innerHTML = questions.length;
     document.getElementById('endResult').innerHTML = rightQuestions;
-    document.getElementById('headerImg').src = './img/trophy.png';
+    document.getElementById('headerImg').src = './img/finish_flag.jpg';
 }
 
 function updateProgressBar() {
@@ -107,7 +263,6 @@ function updateToNextQuestion() {
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
 
 function answer(selection) {
@@ -150,12 +305,10 @@ function resetAnswerButton() {
     document.getElementById('answer_2').parentNode.classList.remove('bg-success');
     document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_3').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
 
 function restartGame() {
-    document.getElementById('headerImg').src = './img/bg.jpg';
+    document.getElementById('headerImg').src = './img/physics_bg.jpg';
     document.getElementById('questionBody').style = '';
     document.getElementById('endScreen').style = 'display: none';
     rightQuestions = 0;
